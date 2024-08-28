bb8e06fccbcfd5dab1892907ef84a582a683fe08
janbolot <kdoublej91@gmail.com>
2024-08-28 19:16:59 +0600

    add tasks category

--- src/test/java/properties/SystemPropertiesTests.java
+++ src/test/java/properties/SystemPropertiesTests.java
@@ -9,7 +9,11 @@ public class SystemPropertiesTests {
     @Tag("one_property")
     public void simplePropertyTest() {
         String browserName = System.getProperty("browser", "mozilla");
+        String browserVersion = System.getProperty("browser_version", "100.0");
 
         System.out.println(browserName);
+        System.out.println(browserVersion);
+
+        // gradle clean one_property_test -Dbrowser=safari -Dbrowser_version=99.0
     }
 }
